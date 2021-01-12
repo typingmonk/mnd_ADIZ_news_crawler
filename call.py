eaa922c64e5b63c4fbbcb957d106e72055cb537b
tmonk <tmonk.hsieh@tmonk.tw>
2021-01-12 20:00:52 +0800

    add config file.

--- call.py
+++ call.py
@@ -1,11 +1,13 @@
 from function.downloadHTML import requestPage 
 from function.downloadFILE import downloadFILE
 from function.extractMap import extractMap
+from config import access_key, secret_access_key
+
 # Test Case:
 # 71207 (無文章、被轉址回首頁)
 # 77871（有文章、是軍機繞台的消息）
 # 71221（有文章、是無關軍機繞台的消息）
-print(requestPage(77871))
-locations = downloadFILE(77871)
-print(locations)
-print(extractMap(77871, locations[1]))
+#print(requestPage(77871))
+#locations = downloadFILE(77871)
+#print(locations)
+#print(extractMap(77871, locations[1]))
---
+++ config.example.py
@@ -0,0 +1,3 @@
+# AWS keys
+access_key = ""
+secret_access_key = ""
